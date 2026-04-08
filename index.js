162ddb927937241d1c92d105a824653b565f8a6a
Carol Kithinji <carolkithinji35@gmail.com>
2026-04-08 16:40:06 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Write your code here!
+//setting up variables
+
+const shoppingList = [
+  {
+    name: "Chicken Breast",
+    price: 10.5,
+  },
+  {
+    name: "Paprika",
+    price: 3.2,
+  },
+  {
+    name: "Chips",
+    price: 6.5,
+  },
+  {
+    name: "Eggs",
+    price: 4.0,
+  },
+  {
+    name: "Milk",
+    price: 7.8,
+  },
+];
+
+function displayCartItem(item) {
+  const shoppingCart = document.querySelector("#cart");
+  const newItem = document.createElement("li");
+  const itemName = document.createElement("p");
+  const itemPrice = document.createElement("p");
+
+  itemName.textContent = item.name;
+  itemPrice.textContent = `$${item.price}`;
+
+  newItem.append(itemName, itemPrice);
+  shoppingCart.append(newItem);
+}
+shoppingList.forEach((item) => {
+  displayCartItem(item);
+});
+
+const form = document.getElementById("new-cart-item-form");
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const newName = event.target.name.value;
+  const newPrice = event.target.price.value;
+  const newItem = {
+    name:newName,
+    price:Number(newPrice),
+  };
+console.log(newName)
+  shoppingList.push(newItem);
+  displayCartItem(newItem);
+}
+
+form.addEventListener("submit", handleSubmit);
+console.log(document)
